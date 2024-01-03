df969ceb272a4c964feda91337cf03e4be5981ae
bddata <heocon1491@gmail.com>
2024-01-04 01:10:51 +0700

    project ltw

---
+++ footer.php
@@ -0,0 +1,121 @@
+
+        <footer class="footer">
+        	<div class="footer-middle">
+	            <div class="container">
+	            	<div class="row">
+	            		<div class="col-sm-6 col-lg-3">
+	            			<div class="widget widget-about">
+	            				<img src="assets/images/sunphonec.png" class="footer-logo" alt="Footer Logo" width="375" height="200">
+                                <p style="font-family:roboto; font-size:16px">Đem kết nối đến tương lai, bạn cứ việc tỏa sáng chúng tôi sẽ đưa bạn đi </p>
+
+
+	            				<div class="widget-call">
+                                    <i class="icon-phone"></i>
+                                   <a style="font-family:roboto; font-size:15px"> Gọi điện cho chúng tôi 24/7
+                                    <a href="tel:#">1800 6601</a>         
+                                </div><!-- End .widget-call -->
+	            			</div><!-- End .widget about-widget -->
+	            		</div><!-- End .col-sm-6 col-lg-3 -->
+
+	            		
+
+	            		<div class="col-sm-6 col-lg-3">
+	            			<div class="widget">
+							<h4   style="font-family:roboto; font-size:20px" class="widget-title">Tin tức  </h4><!-- End .widget-title -->
+
+							<ul class="widget-list">
+								<li><a   style="font-family:roboto; font-size:15px" href="#">Tin tuyển dụng</a></li>
+								<li><a  style="font-family:roboto; font-size:15px" href="#">Tin khuyến mãi</a></li>
+								<li><a  style="font-family:roboto; font-size:15px" href="#">Chính sách trả góp</a></li>
+
+	            				</ul><!-- End .widget-list -->
+	            			</div><!-- End .widget -->
+	            		</div><!-- End .col-sm-6 col-lg-3 -->
+
+	            		<div class="col-sm-6 col-lg-3">
+	            			<div class="widget">
+	            				<h4   style="font-family:roboto; font-size:20px" class="widget-title">Tài khoản </h4><!-- End .widget-title -->
+
+	            				<ul class="widget-list">
+	            					<li><a style="font-family:roboto; font-size:15px" href="index.php">Đăng nhập</a></li>
+	            					<li><a style="font-family:roboto; font-size:15px" href="cart.php">Xem giỏ hàng</a></li>
+	            					<li><a style="font-family:roboto; font-size:15px" href="wishlist.php">Sản phẩm yêu thích</a></li>
+	            					<li><a style="font-family:roboto; font-size:15px" href="#">Đơn hàng</a></li>
+	            				
+	            				</ul><!-- End .widget-list -->
+	            			</div><!-- End .widget -->
+	            		</div><!-- End .col-sm-6 col-lg-3 -->
+						<div class="col-sm-6 col-lg-3">
+	            			<div class="widget">
+							<h4  style="font-family:roboto; font-size:20px" class="widget-title">Chăm sóc khách hàng</h4><!-- End .widget-title -->
+
+							<ul class="widget-list">
+								<li><a style="font-family:roboto; font-size:15px" href="#">Phương thức thanh toán</a></li>
+								<li><a style="font-family:roboto; font-size:15px" href="#">Khám phá ưu đãi</a></li>
+								<li><a style="font-family:roboto; font-size:15px" href="baohanh.php">Chính sách bảo hành</a></li>
+								<li><a style="font-family:roboto; font-size:15px" href="contact.php">Liên hệ</a></li>
+
+															
+	            				</ul><!-- End .widget-list -->
+	            			</div><!-- End .widget -->
+	            		</div><!-- End .col-sm-6 col-lg-3 -->
+	            	</div><!-- End .row -->
+	            </div><!-- End .container -->
+	        </div><!-- End .footer-middle -->
+			
+
+	        <div class="footer-bottom">
+	        	<div class="container">
+	        		<p class="footer-copyright">Copyright © 2022 SunPhone. All Rights Reserved.</p><!-- End .footer-copyright -->
+	        		<figure class="footer-payments">
+	        			<img src="assets/images/payments.png" alt="Payment methods" width="272" height="20">
+	        		</figure><!-- End .footer-payments -->
+	        	</div><!-- End .container -->
+	        </div><!-- End .footer-bottom -->
+        </footer><!-- End .footer -->
+    </div><!-- End .page-wrapper -->
+
+	<button onclick="topFunction()" id="myBtn" title="Go to top"><i class="icon-arrow-up"></i></button>
+
+<style>
+
+#myBtn {
+    display: none;
+    position: fixed;
+    bottom: 10px;
+    right: 43px;
+    z-index: 99;
+    font-size: 18px;
+    border: none;
+    outline: none;
+    background-color: black;
+    color: white;
+    cursor: pointer;
+    padding: 15px;
+    border-radius: 4px;
+}
+
+#myBtn:hover {
+  background-color: #fcb941;
+}
+</style>
+
+
+<script>
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+</script>
